a87f0cc544c5269f58e12d1038fc819045782036
jvargh7 <jvargh7@emory.edu>
2022-05-08 23:07:26 -0400

    Added delta method and marginal prevalence by age

--- models/nlm02_poisson regression urban.R
+++ models/nlm02_poisson regression urban.R
@@ -101,3 +101,39 @@ marginal_predictions <- map_dfr(exposures,function(e){
 })
 
 write_csv(marginal_predictions,paste0("paper/table_marginal predictions from poisson for urban.csv"))
+
+# Marginal date of birth trends --------------
+require(lubridate)
+source("models/delta_method.R")
+overlaps <- readRDS("data/overlaps.RDS")  %>% 
+  dplyr::select(dates,ends_with("_d")) %>% 
+  mutate(month = month(dates),
+         year = year(dates)) %>% 
+  distinct_at(vars(month,year,ends_with("_d")),.keep_all = TRUE)
+
+pred_overlaps <- map_dfr(1:nrow(overlaps), function(i){
+  print(i);
+  e_df = analytic_sample %>% 
+    dplyr::select(-ends_with("_d")) %>% 
+    bind_cols(overlaps[i,-1])
+  
+  
+  pred_stunting = delta_method(glm_stunting,pred_df=e_df)
+  pred_underweight = delta_method(glm_underweight,pred_df=e_df)
+  pred_wasting = delta_method(glm_wasting,pred_df=e_df)
+  
+  
+  
+  data.frame(row = i,
+             mean_pred_e_stunting = pred_stunting[["mean"]],
+             mean_pred_e_underweight = pred_underweight[["mean"]],
+             mean_pred_e_wasting = pred_wasting[["mean"]],
+             se_pred_e_stunting = pred_stunting[["se"]],
+             se_pred_e_underweight = pred_underweight[["se"]],
+             se_pred_e_wasting = pred_wasting[["se"]]
+             ) %>% 
+    return(.)
+})
+
+bind_cols(overlaps,pred_overlaps) %>% 
+  write_csv(.,"paper/prediction for unique months and year for urban.csv")
